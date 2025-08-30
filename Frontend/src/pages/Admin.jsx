d693129c3038b183a018181a5121fd427661e53a
Prathamesh Sharad Mule <159987235+prathameshsmule@users.noreply.github.com>
2025-08-31 00:19:39 +0530

    Update Admin.jsx

--- Frontend/src/pages/Admin.jsx
+++ Frontend/src/pages/Admin.jsx
@@ -46,7 +46,7 @@ const Admin = () => {
   const fetchDonors = async (token) => {
     try {
       const res = await axios.get(
-        `https://www.lifelinebloodcenter.org/api/donors/camp/${selectedCamp}`,
+        `https://www.lifelinebloodcenter.org/donors/camp/${selectedCamp}`,
         {
           headers: { Authorization: `Bearer ${token}` },
         }
