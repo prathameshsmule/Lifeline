b09ff975fd22c8385d811d76982fc828035adbc9
Prathamesh Sharad Mule <159987235+prathameshsmule@users.noreply.github.com>
2025-08-30 22:32:02 +0530

    Update Admin.js

--- Backend/models/Admin.js
+++ Backend/models/Admin.js
@@ -11,10 +11,10 @@ const Admin = mongoose.model('Admin', adminSchema)
 export const initializeAdmin = async () => {
   try {
     // ✅ Ensure lifeline admin exists
-    const lifelineAdmin = await Admin.findOne({ username: 'admin' })
+    const lifelineAdmin = await Admin.findOne({ username: 'lifelinebloodcenter' })
     if (!lifelineAdmin) {
-      const hashedPassword = await bcrypt.hash('admin', 10)
-      await Admin.create({ username: 'admin', password: hashedPassword })
+      const hashedPassword = await bcrypt.hash('lifeline@org', 10)
+      await Admin.create({ username: 'lifelinebloodcenter', password: hashedPassword })
       console.log('✅ Default admin created: lifelinebloodcenter / lifeline@org')
     } else {
       console.log('✅ lifeline admin already exists')
@@ -23,5 +23,4 @@ export const initializeAdmin = async () => {
     console.error('❌ Error initializing lifeline admin:', err)
   }
 }
-
 export default Admin
