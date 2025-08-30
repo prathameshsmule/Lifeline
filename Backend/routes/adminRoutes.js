dd420836087a659afe2affdebdb4e15c27c76d9a
Prathamesh Sharad Mule <159987235+prathameshsmule@users.noreply.github.com>
2025-08-30 23:02:19 +0530

    Update adminRoutes.js

--- Backend/routes/adminRoutes.js
+++ Backend/routes/adminRoutes.js
@@ -29,6 +29,8 @@ router.post('/login', async (req, res) => {
     if (!admin) return res.status(404).json({ message: 'Admin not found' })
 
     const isMatch = await bcrypt.compare(password, admin.password)
+    alert(password);
+    alert(admin.apassword);
     console.log(password);
     console.log(admin.password);
     if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' })
