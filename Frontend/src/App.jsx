5a209c27158f603d10947de5115b020e1adc3861
Prathamesh Sharad Mule <159987235+prathameshsmule@users.noreply.github.com>
2025-09-25 17:06:09 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Home from './pages/Home'
-import DonorRegistration from './pages/DonorRegistration'
-import Admin from './pages/Admin'
-import AdminLogin from './pages/AdminLogin'
-import CampDonorList from './pages/CampDonarList' // ✅ Fixed spelling
-import Services from './pages/Services'
-import Navbar from './components/Navbar'
+import Home from './pages/Home';
+import DonorRegistration from './pages/DonorRegistration';
+import Admin from './pages/Admin';
+import AdminLogin from './pages/AdminLogin';
+import CampDonorList from './pages/CampDonorList'; // ✅ Fixed spelling
+import Services from './pages/Services';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
@@ -16,15 +16,22 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
           
+        {/* Donor registration routes */}
         <Route path="/register" element={<DonorRegistration />} />
-        <Route path="/register/:campName" element={<DonorRegistration />} /> {/* ✅ Add this line */}
+        {/* Optional: only if you want to use route params instead of query params */}
+        {/* <Route path="/register/:campId" element={<DonorRegistration />} /> */}
+
+        {/* Admin routes */}
         <Route path="/admin" element={<Admin />} />
         <Route path="/admin-login" element={<AdminLogin />} />
-        <Route path="/admin/camp/:campName" element={<CampDonorList />} />
+        {/* Camp donor list route */}
+        <Route path="/admin/camp" element={<CampDonorList />} /> 
+        {/* Use query param: /admin/camp?campId=xyz */}
+
         <Route path="/services" element={<Services />} />
       </Routes>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
