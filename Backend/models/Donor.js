2f446530bc210f77d6f086f0a057681a8d7aba23
Prathamesh Sharad Mule <159987235+prathameshsmule@users.noreply.github.com>
2025-09-26 10:32:10 +0530

    Update Donor.js

--- Backend/models/Donor.js
+++ Backend/models/Donor.js
@@ -1,15 +1,16 @@
+// models/Donor.js
 import mongoose from 'mongoose'
 
 const donorSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  dob: { type: Date, required: true },
+  dob: { type: Date },                // optional now
   age: { type: Number, required: true },
   weight: { type: Number, required: true },
   bloodGroup: { type: String, required: true },
   email: { type: String, required: true },
   phone: { type: String, required: true },
   address: { type: String, required: true },
-  camp: { type: mongoose.Schema.Types.ObjectId, ref: 'Camp', required: true }, // ✅ Store camp _id
+  camp: { type: mongoose.Schema.Types.ObjectId, ref: 'Camp', required: true },
   remark: { type: String, default: '' }
 }, { timestamps: true })
 
